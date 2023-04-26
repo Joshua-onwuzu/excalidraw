641fc9ecd7e0be0df69e9960b70d0ef676b8eea1
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2023-04-26 08:20:59 +0100

    docs: release @excalidraw/excalidraw@1.1.7  🎉

--- src/excalidraw-app/index.tsx
+++ src/excalidraw-app/index.tsx
@@ -711,12 +711,28 @@ const ExcalidrawWrapper = ({
             return null;
           }
           return (
-            <div>
+            <div className="flex">
               <LiveCollaborationTrigger
                 isCollaborating={isCollaborating}
                 onSelect={() => setCollabDialogShown(true)}
               />
-              <span>SPPOON </span>
+              <button
+                className={`bg-black text-white flex items-center justify-center box-border px-6 py-2 rounded-md`}
+              >
+                <svg
+                  width="14"
+                  height="18"
+                  viewBox="0 0 14 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 13.5H10V7.5H14L7 0.5L0 7.5H4V13.5ZM7 3.33L9.17 5.5H8V11.5H6V5.5H4.83L7 3.33ZM0 15.5H14V17.5H0V15.5Z"
+                    fill="white"
+                  />
+                </svg>
+                <p>Publish</p>
+              </button>
             </div>
           );
         }}
